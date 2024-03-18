311f625377ca068f50df6fdaf594f6ae65e592e7
Maxime Jochum <mjochum@c2r3p9.42perpignan.fr>
2024-03-18 12:35:23 +0100

    i dont know what im doing

--- C00/ex01/Phonebook.class.hpp
+++ C00/ex01/Phonebook.class.hpp
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Phonebook.class.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjochum <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/17 08:38:22 by mjochum           #+#    #+#             */
-/*   Updated: 2024/03/18 12:18:05 by mjochum          ###   ########.fr       */
+/*   Created: 2024/03/18 11:44:59 by mjochum           #+#    #+#             */
+/*   Updated: 2024/03/18 11:48:04 by mjochum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 
-void ft_uppercase(std::string str)
-{
-	for (int i = 0; str[i] != '\0'; i++)
-		str[i] = toupper(str[i]);
-	std::cout << str;
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);	
-	}
-	for (int i = 1; i < argc; i++)
-		ft_uppercase(argv[i]);
-	std::cout << std::endl;
-}
